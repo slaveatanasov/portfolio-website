4206f337f967b11b3742dc2abe4a92a3fbfed7bb
Slave Atanasov <slaveatanasovmusic@gmail.com>
2020-03-29 14:50:35 +0200

    Cleaned tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          lighter: 'hsl(207, 73%, 52%)',
-          default: 'hsl(207, 73%, 57%)',
-          darker: 'hsl(207, 73%, 44%)'
-        }
-      }
-    }
-  },
+  theme: {},
   variants: {},
   plugins: []
 };
