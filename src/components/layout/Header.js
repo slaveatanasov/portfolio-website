e5b5f1cb7321b625ebda91b11d612c72076d5995
Slave Atanasov <slaveatanasovmusic@gmail.com>
2020-03-30 17:32:55 +0200

    Fixed header z-index.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Header = () => (
-  <header className="sticky top-0 bg-white shadow">
+  <header className="navigation-menu sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
         <div className="mr-3">
